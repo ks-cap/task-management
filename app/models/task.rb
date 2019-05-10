d0333f4db3d2a48e0d32e77459dfcae9684e523e
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-10 18:43:46 +0900

    Show only data for logged in user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+
+  validate :validate_name_not_including_comma
+
+  # UserとTaskは１対多の関係
+  belongs_to :user
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
