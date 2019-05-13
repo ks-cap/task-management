3453a98bd65bfff0226064721d44255556ec367c
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-13 10:36:06 +0900

    Consideration on Error of Update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,8 +27,10 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
   end
 
   def destroy
