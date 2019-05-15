c053271983862caa706600dd9b57bcfa0f1531c4
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-15 16:44:13 +0900

    Add information for pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   def index
     # ログインしているユーザーに紐づくTaskだけを表示
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page])
   end
 
   def show
