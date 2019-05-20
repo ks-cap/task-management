fde53913dfc8581319eb5fce03e9a74b3b4beada
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-20 17:02:25 +0900

    Add validate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_071121) do
+ActiveRecord::Schema.define(version: 2019_05_20_074330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2019_05_20_071121) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
     t.text "description"
@@ -46,6 +52,15 @@ ActiveRecord::Schema.define(version: 2019_05_20_071121) do
     t.string "state", default: "未着手", null: false
     t.integer "owner_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_user_groups_on_group_id"
+    t.index ["user_id"], name: "index_user_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +74,6 @@ ActiveRecord::Schema.define(version: 2019_05_20_071121) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "user_groups", "groups"
+  add_foreign_key "user_groups", "users"
 end
