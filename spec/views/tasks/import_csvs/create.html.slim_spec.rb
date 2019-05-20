eb296b24b35d6b403e38f50611a12f5194bad16f
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-20 13:45:44 +0900

    Add test code when deselect file

--- spec/views/tasks/import_csvs/create.html.slim_spec.rb
+++ spec/views/tasks/import_csvs/create.html.slim_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "import_csvs/create.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'CSV形式のファイルをインポート・エクスポート', type: :view do
+
+  before do
+    # ログインする
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  describe 'インポート機能' do
+    context 'ファイルを選択せずにインポートボタンを押したとき' do
+      before do
+        click_button 'インポート'
+      end
+
+      it 'エラーとなる' do
+        expect(page).to have_selector '.alert-danger', text: 'CSVによるタスク一括登録に失敗しました(ファイルを指定して下さい)'
+      end
+    end
+  end
 end
