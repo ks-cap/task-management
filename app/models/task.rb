1d3b65b4364a6dea08bcc0e7aee6bee3c8a31dc0
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-23 19:04:40 +0900

    🚨

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  #enum state: { 未着手: '未着手', '着手中': '着手中', 完了: '完了' }
+  # enum state: { 未着手: '未着手', '着手中': '着手中', 完了: '完了' }
   enum state: { waiting: 0, working: 1, completed: 2 }
   belongs_to :user
   belongs_to :owner, class_name: 'User'
