617a022565a0638d153af084cdc29560c67e9e05
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-23 13:30:10 +0900

    削除アクションに対しdependentを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
-  has_many :user_groups
+  has_many :user_groups, dependent: :destroy
   has_many :users, through: :user_groups
 
   validates :name, presence: true, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # UserとTaskは１対多の関係
   has_many :tasks
   has_many :owner_tasks, class_name: 'Task', foreign_key: :owner_id
-  has_one :user_group
+  has_one :user_group, dependent: :destroy
   has_one :group, through: :user_group
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
