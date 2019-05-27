556c167c88c936032adcee9fee0d376d6244a18f
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-27 20:59:57 +0900

    製作中

--- app/models/task.rb
+++ app/models/task.rb
@@ -24,11 +24,11 @@ class Task < ApplicationRecord
 
   # ransack使用時の制約追加
   def self.ransackable_attributes(_auth_object = nil)
-    %w[name created_at deadline state owner_id]
+    %w[name created_at deadline state owner_id tag_list_eq]
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    %w[user]
+    %w[user tags]
   end
 
   def self.csv_attributes
