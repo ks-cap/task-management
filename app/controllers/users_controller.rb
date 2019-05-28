564595eb0cb45c251cf164e02a4d3b28f6e63c22
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-28 17:39:40 +0900

    アカウントをログイン前から作成できるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
+  skip_before_action :login_required, only: %i[new create]
+
   USER_DISPLAY_PER_PAGE = 25
 
   def index
