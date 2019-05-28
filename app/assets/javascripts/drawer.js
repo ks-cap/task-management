8674cf3e2293c80970e930d26590ecd4b0e740c7
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-28 17:51:29 +0900

    最初の画面のみ新規作成ボタン設置

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
     $('.drawer').drawer();
-    // ドロワーメニューが開いたとき
-    $('.drawer').on('drawer.opened', function(){
-        alert('ドロワーが開きました');
-    });
-
-    // ドロワーメニューが閉じたとき
-    $('.drawer').on('drawer.closed', function(){
-        alert('ドロワーが閉じられました');
-    });
 });
