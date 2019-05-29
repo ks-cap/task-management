206e6fffb7fbd6cd333deece0453d74a8f3cae37
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-29 16:24:24 +0900

    イベント表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   get 'calendars/index'
+  get 'events', to: 'event#events'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
